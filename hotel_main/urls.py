82117951142ecb30f2eeb02f3e84fff9d9b2283d
RamirezJosue <xdemobtkx@gmail.com>
2017-10-27 16:41:28 -0500

    api rest de categorias

--- hotel_main/urls.py
+++ hotel_main/urls.py
@@ -1,18 +1,3 @@
-"""hotel_main URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf import settings
@@ -23,4 +8,5 @@ urlpatterns = [
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
     url(r'^api-core/', include('core.urls')),
+    url(r'^api-hotel/', include('hotel.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
